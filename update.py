f45f2fd44728d6a8c7f81e50f37cce8743ec3c02
jgo42 <68195787+jgo42@users.noreply.github.com>
2023-06-30 14:52:28 +0600

    Update update.py

--- update.py
+++ update.py
@@ -53,7 +53,7 @@ if DATABASE_URL:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = 'https://github.com/5hojib/tanha'
+    UPSTREAM_REPO = 'https://github.com/5hojib/wzml-x'
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
